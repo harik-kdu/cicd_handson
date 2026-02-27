402a6e4c4bbfa07b25af68a0a691a3bae61e0d1b
harik-kdu <hari.k@kickdrum.com>
2026-02-27 11:44:31 +0530

    Initial Commit

---
+++ src/main/java/com/cicdpieline/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.cicdpieline.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    
+    @Value("${application.version:unknown}")
+    private String version;
+
+    @RequestMapping("/version")
+    public String version() {
+        return "Version: " + version;
+    }
+
+}
